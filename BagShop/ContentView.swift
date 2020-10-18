42229d25d5a9b6220605615f94b17d1d4d4bcfa4
Akin <rapiddevelopa@gmail.com>
2020-10-18 14:39:56 +0100

    Initial Commit

---
+++ BagShop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BagShop
+//
+//  Created by Admin on 10/18/20.
+//  Copyright © 2020 rapid interactive. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
